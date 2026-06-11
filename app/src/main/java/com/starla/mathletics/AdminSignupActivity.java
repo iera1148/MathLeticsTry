4ad05bc1e37a44baf81e32cb0f85baffc65e9381
mimiSnacky <mizasharmiza739@gmail.com>
2026-06-11 16:21:38 +0800

    tambah komen test lagi

--- app/src/main/java/com/starla/mathletics/AdminSignupActivity.java
+++ app/src/main/java/com/starla/mathletics/AdminSignupActivity.java
@@ -13,7 +13,7 @@ import com.starla.mathletics.models.User;
 public class AdminSignupActivity extends AppCompatActivity {
 
     private DatabaseHelper db;
-    //test lagi
+    //test lagi dan lagi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
